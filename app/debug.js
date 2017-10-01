791d815dc207037707b7d1f2a4f995888cb0f15f
Sunny <sunny.lan.coder@gmail.com>
2017-10-01 18:12:16 -0400

    Debug module refactored

--- app/debug.js
+++ app/debug.js
@@ -3,10 +3,10 @@ const JSON = require('circular-json');
 //handles a client websocket connection
 require('colors');
 module.exports = (engine) => {
-    engine.onM(['*', '*', '*', '**'], (state, next, payload, evt) => {
+    engine.on(['*', '*', '*', '**'], (payload, evt) => {
         const color = evt.name === 'errorOccurred' ? 'red' : 'yellow';
         if (debugLevel === 'none')
-            next(state);
+           return;
         else if (debugLevel === 'short') {
             console.log(("Event occurred:" + evt.name
                 + '\n' + ' direction: ' + evt.src + '->' + evt.dst
@@ -28,6 +28,5 @@ module.exports = (engine) => {
         }
         if (color === 'red')
             console.log(payload.err.message.red);
-        next(state);
     });
 };
